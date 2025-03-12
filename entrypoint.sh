f7412cf4ee4055abc5d9070eafc23e79e48608c4
MichalS-94 <64809601+MichalS-94@users.noreply.github.com>
2025-03-12 11:31:50 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 
 cleanup() {
-	echo "Exiting..."
-	exit 0
+    echo "Exiting..."
+    exit 0
 }
 
 trap cleanup SIGINT
 trap cleanup SIGTERM
 
+# Load environment variables from /home/config if the file exists
+CONFIG_FILE="/home/config"
+if [ -f "$CONFIG_FILE" ]; then
+    echo "Loading environment variables from $CONFIG_FILE"
+    export $(grep -v '^#' "$CONFIG_FILE" | xargs)
+fi
+
 # Exit if BACKUP_TARGETS is not set
 if [ -z "$BACKUP_TARGETS" ]; then
-	echo "BACKUP_TARGETS is not set. Set in the form of 'root.pxar:/' or several separated by space, like 'etc.pxar:/etc var.pxar:/var'. Exiting."
-	exit 1
+    echo "BACKUP_TARGETS is not set. Set in the form of 'root.pxar:/' or several separated by space, like 'etc.pxar:/etc var.pxar:/var'. Exiting."
+    exit 1
 fi
 
 # Exit if no CRON_SCHEDULE is set
 if [ -z "$CRON_SCHEDULE" ]; then
-	echo "CRON_SCHEDULE is not set. Set in the form of Cron timing, e.g. '00 03 * * *' for 3am every day.  Exiting."
-	exit 1
+    echo "CRON_SCHEDULE is not set. Set in the form of Cron timing, e.g. '00 03 * * *' for 3am every day.  Exiting."
+    exit 1
 fi
 
 env > /cronfile
@@ -32,5 +39,5 @@ echo "Starting backup service..."
 
 # Loop forever
 while true; do
-	sleep 1
+    sleep 1
 done
