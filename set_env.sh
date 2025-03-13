64c78b9515ff34014023c1f714cf115f9348997e
MichalS-94 <64809601+MichalS-94@users.noreply.github.com>
2025-03-13 15:14:29 +0100

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
-
-CONFIG_FILE="/home/config"
-
-# Check if the config file exists and is readable
-if [ -f "$CONFIG_FILE" ]; then
-    echo "Loading environment variables from $CONFIG_FILE"
-    
-    # Automatically export variables from the config file
-    set -a  # Automatically export variables
-    . "$CONFIG_FILE"
-    set +a  # Stop automatically exporting variables
-else
-    echo "Config file not found: $CONFIG_FILE"
+ 
+# Load environment variables from /home/config if the file exists
+if [ -f /home/config ]; then
+    echo "Loading environment variables from /home/config"
+    export $(grep -v '^#' /home/config | xargs)
 fi
