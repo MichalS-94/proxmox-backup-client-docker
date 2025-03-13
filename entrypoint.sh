afa968c852ea289b67859feaa597a8b9426fc1f5
MichalS-94 <64809601+MichalS-94@users.noreply.github.com>
2025-03-13 14:59:16 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,10 @@ cleanup() {
 trap cleanup SIGINT
 trap cleanup SIGTERM
 
-set -e
 # Set env if exists
 if [ -f "/set_env.sh" ]; then
     . /set_env.sh
 fi
-
-exec "$@"
 
 # Exit if BACKUP_TARGETS is not set
 if [ -z "$BACKUP_TARGETS" ]; then
